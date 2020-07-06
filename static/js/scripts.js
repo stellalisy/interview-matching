560fa2f7a9c1028d7d5253ed0c3c9176a26ea63a
Stella <stellaaalisy@gmail.com>
2020-07-06 20:50:52 +0800

    removed unnecessary files

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -114,17 +114,61 @@ var content = "";
 var num = 1;
 for (var i = 1; i <= rows; i++) {
   for (var j = 1; j <= cols; j++) {
-    if (j === 1) {
-      num = (j-1) * cols + i;
-      content += "<div style='text-align:center;' class='row'><div class='grid'>" + num + "</div>";
+    if (j === 1) {;
+      time_slot = i - 1;
+      if (i === 1){
+        content += "<div class='row'><div style='text-align:center;background:#FFFFFF;color:#000000' class='grid_label'></div>";
+      } else {
+      content += "<div class='row'><div style='text-align:center;background:#FFFFFF;color:#000000' class='grid_label'>" + "time " + time_slot + "</div>";
+      }
     } else if (j === cols) {
-      num = (j-1) * cols + i;
-      content += "<div style='text-align:center;' class='grid'>" + num + "</div></div>";
+      if (i === 1){
+        day = j - 1;
+        content += "<div style='text-align:center;background:#FFFFFF;color:#000000' class='grid_label'>" + "Day " + day +  "</div></div>";
+      }
+      else {
+        num = (j-2) * (rows - 1) + i - 1;
+        content += "<div style='text-align:center;' class='grid'>" + num +  "</div></div>";
+      }
     } else {
-      num = (j-1) * cols + i;
-      content += "<div style='text-align:center;' class='grid'>" + num + "</div>";
+      if (i === 1){
+        day = j - 1;
+        content += "<div style='text-align:center;background:#FFFFFF;color:#000000' class='grid_label'>" + "Day " + day +  "</div>";
+      }
+      else {
+        num = (j-2) * (rows - 1) + i - 1;
+        content += "<div style='text-align:center;' class='grid'>" + num +  "</div>";
+      }
     }
     //num++;
   }
 }
 $("#grids").html(content);
+
+$("#grids").on("click", ".grid", function() {
+  var value = $(this).text();
+  setColor(event);
+});
+
+function setColor(e) {
+  var target = e.target,
+      count = +target.dataset.count;
+
+   target.style.backgroundColor = count === 1 ? "#adade4" : '#c2e4ad';
+   target.dataset.count = count === 1 ? 0 : 1;
+   /* 
+
+   () : ? - this is conditional (ternary) operator - equals 
+
+   if (count === 1) {
+      target.style.backgroundColor = "#7FFF00";
+      target.dataset.count = 0;
+   } else {
+      target.style.backgroundColor = "#FFFFFF";
+      target.dataset.count = 1;
+   } 
+   target.dataset - return all "data attributes" for current element, 
+   in the form of object, 
+   and you don't need use global variable in order to save the state 0 or 1
+  */ 
+}
