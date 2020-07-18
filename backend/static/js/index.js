87d04936d24d0c365b1af6acd7af5c1e550482f1
Stella <stellaaalisy@gmail.com>
2020-07-18 19:03:32 +0800

    reorganized repo

--- backend/static/js/index.js
+++
@@ -1,2 +0,0 @@
-require('./app.js');
-alert('index works');
--- frontend/src/components/admin/Admin.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { withAdminAuth } from "../../util/auth";
-
-const Admin =  function() {
-    return (<div> Admin Page </div>);
-};
-
-export default withAdminAuth(Admin);
