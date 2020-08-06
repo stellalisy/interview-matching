16a0af66876211533f8c4bc5ad98e4e166ef2f68
Stella <stellaaalisy@gmail.com>
2020-08-06 10:23:03 +0800

    removed extra jsx files

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import Interview from './components/Interview';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Interview />, document.getElementById('root'));
